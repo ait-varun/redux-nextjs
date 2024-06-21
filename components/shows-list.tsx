1674b5e3f2ffbddc78e153773f9ceb55a5937fe5
Varun <aitvarunbhatia@gmail.com>
2024-06-21 17:05:20 +0530

    updated list

--- components/shows-list.tsx
+++ components/shows-list.tsx
@@ -43,37 +43,39 @@ export default function ShowsList() {
                   <div className="bg-white shadow overflow-hidden sm:rounded-md">
                     <ul className="divide-y divide-gray-200 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 2xl:grid-cols-6 gap-4 p-4">
                       {currentData.map((movie) => (
-                        <Link href={`/shows/${movie.id}`} key={movie.id}>
+                        <>
                           {" "}
                           <li className="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300">
-                            <div className="flex flex-col justify-center">
-                              <div className="w-full">
-                                <Image
-                                  src={movie.image.original}
-                                  alt={movie.name}
-                                  width={500}
-                                  height={300}
-                                  priority
-                                  className="w-full h-auto min-h-96 max-h-96 rounded-t-lg object-fill"
-                                />
+                            <Link href={`/shows/${movie.id}`} key={movie.id}>
+                              <div className="flex flex-col justify-center">
+                                <div className="w-full">
+                                  <Image
+                                    src={movie.image.original}
+                                    alt={movie.name}
+                                    width={500}
+                                    height={300}
+                                    priority
+                                    className="w-full h-auto min-h-96 max-h-96 rounded-t-lg object-fill"
+                                  />
+                                </div>
+                                <div className="mt-2 mb-2 ps-6">
+                                  <p className="text-xl font-bold text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis">
+                                    {movie.name}
+                                  </p>
+                                  <p className="text-sm font-medium text-gray-600 overflow-hidden whitespace-nowrap text-ellipsis">
+                                    Rating: {movie.rating.average}
+                                  </p>
+                                  <p className="text-sm font-medium text-gray-600 overflow-hidden whitespace-nowrap text-ellipsis">
+                                    Country: {movie.network?.country?.name}
+                                  </p>
+                                  <p className="text-sm font-medium text-gray-600 overflow-hidden whitespace-nowrap text-ellipsis">
+                                    Runtime: {movie.runtime} minutes
+                                  </p>
+                                </div>
                               </div>
-                              <div className="mt-2 mb-2 ps-6">
-                                <p className="text-xl font-bold text-gray-900 overflow-hidden whitespace-nowrap text-ellipsis">
-                                  {movie.name}
-                                </p>
-                                <p className="text-sm font-medium text-gray-600 overflow-hidden whitespace-nowrap text-ellipsis">
-                                  Rating: {movie.rating.average}
-                                </p>
-                                <p className="text-sm font-medium text-gray-600 overflow-hidden whitespace-nowrap text-ellipsis">
-                                  Country: {movie.network?.country?.name}
-                                </p>
-                                <p className="text-sm font-medium text-gray-600 overflow-hidden whitespace-nowrap text-ellipsis">
-                                  Runtime: {movie.runtime} minutes
-                                </p>
-                              </div>
-                            </div>
+                            </Link>
                           </li>
-                        </Link>
+                        </>
                       ))}
                     </ul>
                   </div>
