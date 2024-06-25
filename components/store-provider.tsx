511b452d02dc04622a22f02e30f795c3bf9a5534
Varun <aitvarunbhatia@gmail.com>
2024-06-25 12:53:25 +0530

    updated tests

--- components/store-provider.tsx
+++ components/store-provider.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import { Provider } from "react-redux";
-import { store } from "@/store/store";
+import { setupStore } from "@/store/store";
 
 export default function StoreProvider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const store = setupStore();
   return (
     <>
       <Provider store={store}>{children}</Provider>
